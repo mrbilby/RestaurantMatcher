4a81d2d6dbdb518c823a3ff1799b2f4d78d4a27c
mrbilby <baileymrb@gmail.com>
2023-04-24 20:03:15 +0100

    Initial Commit

---
+++ RestaurantMatcher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RestaurantMatcher
+//
+//  Created by James Bailey on 24/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestaurantMatcher/RestaurantMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMatcherApp.swift
+//  RestaurantMatcher
+//
+//  Created by James Bailey on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
